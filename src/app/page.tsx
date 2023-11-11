33463b40ef31f0c97225a4f734bdba69b5909fa3
Darlan Bomfim <darlanbs05@gmail.com>
2023-11-11 19:42:50 -0300

    :sparkles: Feat:Criação do footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Footer from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 import { fetchApiData } from "@/services/database";
 import { useState, useEffect } from "react";
@@ -19,6 +20,7 @@ export default function Home() {
       <main>
         <p>tesste</p>
       </main>
+      <Footer />
     </>
   );
 }
