e3d8be1b0848b442c660f65c5e6af25723878304
Darlan Bomfim <darlanbs05@gmail.com>
2023-11-12 09:06:10 -0300

    :boom: Fix: Corrigindo font de acordo com figma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MKS Desafio Tecnico",
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{ margin: 0 }}>
+      <body className={montserrat.className} style={{ margin: 0 }}>
         {children}
       </body>
     </html>
