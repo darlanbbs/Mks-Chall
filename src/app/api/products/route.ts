6772aae8c5df4b9784bc2a67bd087d0807d193ac
Darlan Bomfim <darlanbs05@gmail.com>
2023-11-12 09:28:43 -0300

    :boom: Fix: Corrigindo quantidade recebida vindo da api de acordo com figma

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const res = await fetch(
-    "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=9&sortBy=name&orderBy=DESC"
+    "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=name&orderBy=DESC"
   );
 
   if (!res.ok) {
