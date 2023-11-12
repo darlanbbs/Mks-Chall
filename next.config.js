95bfd73e6eda236b1c1f6e1b1178638a75609ed8
Darlan Bomfim <darlanbs05@gmail.com>
2023-11-12 10:27:23 -0300

    :boom: Fix: Alteração do remote patterns da config do next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mks-sistemas.nyc3.digitaloceanspaces.com",
+      },
+    ],
+  },
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
