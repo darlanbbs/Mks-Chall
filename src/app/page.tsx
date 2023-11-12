1ffbeed567b0734737f33ddf1a644f5684fd22e8
Darlan Bomfim <darlanbs05@gmail.com>
2023-11-12 12:53:47 -0300

    :sparkles: Feat: Implementando o Provider do redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,21 @@ import Footer from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 import { ThemeProvider } from "styled-components";
 import { Container } from "./GlobalStyle";
+import { store } from "./../store/index";
+import { Provider } from "react-redux";
 
 import CatalogProduct from "@/components/CatalogProduct/CatalogProduct";
 
 export default function Home() {
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Header />
-        <CatalogProduct />
-        <Footer />
-      </Container>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Header />
+          <CatalogProduct />
+          <Footer />
+        </Container>
+      </ThemeProvider>
+    </Provider>
   );
 }
