95f96a8988ef543c30251d1dc617eef7913185e4
Darlan Bomfim <darlanbs05@gmail.com>
2023-11-12 15:16:47 -0300

    :recycle: refactor: refatorando catalogo de produtos

--- src/components/CatalogProduct/CatalogProduct.tsx
+++ src/components/CatalogProduct/CatalogProduct.tsx
@@ -2,8 +2,6 @@ import { ArrProduct } from "@/@Types/Types";
 import { fetchApiData } from "@/services/database";
 import React, { useEffect, useState } from "react";
 import ProductsCard from "../ProductsCard/ProductsCard";
-import { Skeleton } from "../Skeleton/styles";
-import { CatalogArea, Container } from "./styles";
 
 type Props = {};
 
@@ -18,24 +16,7 @@ const CatalogProduct = (props: Props) => {
   return (
     <Container>
       <CatalogArea>
-        {data.length > 0 ? (
-          data.map((product: any) => (
-            <div key={product.id}>
-              <ProductsCard
-                brand={product.brand}
-                quantity={product.quantity}
-                total={product.total}
-                id={product.id}
-                name={product.name}
-                description={product.description}
-                photo={product.photo}
-                price={product.price}
-              />
-            </div>
-          ))
-        ) : (
-          <Skeleton />
-        )}
+        <ProductsCard product={data} />
       </CatalogArea>
     </Container>
   );
