1665811982c27d0e052aaeaad6edbdd833847e4e
Darlan Bomfim <darlanbs05@gmail.com>
2023-11-12 09:17:39 -0300

    :recycle: refactor: refatorando arquivo de pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,17 @@
 import { theme } from "@/Theme/theme";
 import Footer from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
-import { fetchApiData } from "@/services/database";
-import { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { Container } from "./GlobalStyle";
-import { ArrProduct } from "@/@Types/Types";
+
+import CatalogProduct from "@/components/CatalogProduct/CatalogProduct";
 
 export default function Home() {
-  const [data, setData] = useState<ArrProduct[]>([]);
-
-  useEffect(() => {
-    fetchApiData().then((data) => {
-      setData(data.data);
-    });
-  }, []);
-  console.log(data);
-
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Header />
-        <p>tesste</p>
+        <CatalogProduct />
         <Footer />
       </Container>
     </ThemeProvider>
